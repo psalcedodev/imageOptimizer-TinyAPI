cbbc1c276f546d35115e15953518786d3175a676
Paul Salcedo <psalcedodev@gmail.com>
2021-01-15 17:54:21 -0700

    Optimizes images success

---
+++ index.js
@@ -0,0 +1,17 @@
+const tinify = require("tinify");
+tinify.key = "TygHzNB9wKNYKwrcljyRpdXdLvqczbsX";
+
+const imgFolder = './images';
+const fs = require('fs');
+
+fs.readdir(imgFolder, (err, files) => {
+  files.forEach(file => {
+    const source = tinify.fromFile(`images/${file}`);
+    try{
+        source.toFile(`./optimizedImg/optimized_${file}`);
+
+    }catch(e){
+        console.log(e)
+    }
+  });
+});
